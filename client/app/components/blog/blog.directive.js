fce48ff7b5e15e4337133c89968bf024d8e050e9
Daniel Rasmuson <dan123911@gmail.com>
2015-10-17 20:22:27 -0700

    init

---
+++ client/app/components/blog/blog.directive.js
@@ -0,0 +1,14 @@
+import './blog.css';
+import template from './blog.html';
+import {BlogController as controller} from './blog.controller';
+
+export const blogDirective = () => {
+  return {
+    template,
+    controller,
+    controllerAs: 'vm',
+    scope: {},
+    restrict: 'E',
+    replace: true
+  };
+};
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var autoprefixer = require('autoprefixer');
+var precss      = require('precss');
+
+/*
+config for webpack. Will be used in
+the Gulpfile for building our app.
+Does not need gulp in order to do so,
+but we use gulp to orchestrate
+ */
+module.exports = {
+  output: {
+    filename: 'bundle.js'
+  },
+
+  devtool: 'sourcemap',
+
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+      { test: /\.(png|jpg|jpeg)$/, loader: 'file' },
+      { test: /\.js$/, loader: 'babel?stage=1', exclude: [/client\/lib/, /node_modules/, /\.spec\.js/] }
+    ]
+  },
+
+  postcss: function () {
+    return [autoprefixer, precss];
+  }
+
+};
